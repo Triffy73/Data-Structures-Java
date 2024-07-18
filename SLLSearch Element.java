e85beb13481ab24f035f61f54d530f956326e8a0
Triffy73 <triphenatriphy@gmail.com>
2024-07-18 21:32:33 +0530

    Create SLLSearch Element.java

---
+++ SLLSearch Element.java
@@ -0,0 +1,75 @@
+import java.util.*;
+public class Main {
+    Node head;
+
+    class Node {
+        int data;
+        Node next;
+        //Node head;
+
+        Node(int val) {
+            data = val;
+            next = null;
+            //head = null;
+        }
+    }
+
+    Main() {
+        head = null;
+    }
+
+    public void insertend(int val) {
+        Node newnode = new Node(val);
+
+        if (head == null) {
+            head = newnode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newnode;
+        }
+    }
+
+    public void search(int k) {
+        Node temp = head;
+        int c=0;
+        while (temp != null) {
+            if (k == temp.data) {
+                System.out.print("Found");
+                c++;
+                break;
+            } else
+                temp = temp.next;
+        }
+        if(c==0)
+            System.out.println("Not found");
+    }
+
+    public void display() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + "-->");
+            temp = temp.next;
+        }
+        System.out.print("Null");
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Main list = new Main();
+        System.out.println("Enter the size of node:");
+        int m = sc.nextInt();
+        System.out.println("Enter the values:");
+        for (int i = 0; i < m; i++) {
+            int val = sc.nextInt();
+            list.insertend(val);
+        }
+        list.display();
+        System.out.println("\nEnter the key value:");
+        int k = sc.nextInt();
+        list.search(k);
+    }
+}
